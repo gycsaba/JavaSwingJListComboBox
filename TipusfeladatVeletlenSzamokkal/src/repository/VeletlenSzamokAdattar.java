af41906638680cf5202b2a6049cbf505e8f8699b
Csaba Gyuris <gycsaba@vasvari.hu>
2019-03-01 09:49:50 +0100

    Gyakorlo feladat elkészült

---
+++ TipusfeladatVeletlenSzamokkal/src/repository/VeletlenSzamokAdattar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author gycsaba
+ */
+public class VeletlenSzamokAdattar {
+    List<Integer> szamok;
+    
+    public VeletlenSzamokAdattar() {
+        szamok=new ArrayList();
+    }
+    
+    public void generalSzamok(int veletlenSzamokSzama) {
+        
+    }
+    
+    public List<Integer> getSzamok() {
+        return szamok;
+    }
+    
+    public double getAtlag() {
+        return 0.0;
+    }
+            
+    
+}
